0b704dd58eab024b2738702ea9001c2e76c2fb43
Priyanshu12220066 <120674056+Priyanshu12220066@users.noreply.github.com>
2024-06-16 10:38:10 +0530

    Create LECTURE07.cpp

---
+++ LECTURE07.cpp
@@ -0,0 +1,11 @@
+IN THIS LECTURE I HAVE LEARNT ABOUT ARRAYS.
+ARRAYS - IT IS COLLECTION OF ELEMENTS OF SAME TYPE PLACED IN CONTIGUOUS MEMORY LOCATIONS.USED TO STORE MULTIPLE INTEGERS UNDER A SINGLE VARIABLE NAME.
+syntax: type array-name[size];
+where type means data-type.
+  
+CODE:
+#include<iostream>;
+using namespace std;
+int main(){
+  int a[5];   //array named "a", which can store 6 integers(0-5).
+  
