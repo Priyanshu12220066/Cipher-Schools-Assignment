6eecb84c64fa0ada7612f11900351852e9768331
Priyanshu12220066 <120674056+Priyanshu12220066@users.noreply.github.com>
2024-06-18 12:58:12 +0530

    Create LECTURE14.cpp

---
+++ LECTURE14.cpp
@@ -0,0 +1,43 @@
+THIS IS THE SECOND INTRODUCTION VIDEO OF FUNCTIONS.
+
+int compare(int a, int b);  //HERE COMPARE IS NAME OF THE FUNCTION AND  a & b  ARE THE INPUT VALUES WHICH ARE OF TYPE INT.
+
+VOID IS A RETURN TYPE IN WHICH NOTING IS RETURNED.
+EXAMPLE:- 
+#include<iostream>
+using namespace std;
+
+void printStuff(){
+  cout<<"Hellooo this is priyanshu";
+]
+
+int main(){
+  printStuff();
+  retuen 0;
+}
+
+EVERY FUNCTION CAN RETURN ONLY ONE VALUE BUT CAN HAVE MULTIPLE RETURN STATEMENTS.
+
+
+Q) WRITE A FUNCTION TO ADD TWO NUMBERS?
+
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b){
+  int c = a+b;
+  return c;
+}
+
+int main(){
+  int x=3;
+  int y=4;
+  sum(x,y);
+  return 0;
+}
+  
+
+
+  
+
+
